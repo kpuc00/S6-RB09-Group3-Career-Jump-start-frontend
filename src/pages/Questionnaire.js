79d7c11c392f7c5a04d1fb5f88e42826f1454007
Kristiyan Strahilov <kdstrahilov@gmail.com>
2022-06-02 11:47:28 +0200

    fixes

--- src/pages/Questionnaire.js
+++ src/pages/Questionnaire.js
@@ -13,7 +13,6 @@ import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { useSelector, useDispatch } from "react-redux";
 import {
   getSF,
-  selectQuestions,
   selectQuestionsAnswered,
   selectSoftFactors,
 } from "../features/softfactor/softfactorSlice";
